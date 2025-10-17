2fb8357447fc923df86666043bc6c5b0b7eb1008
Thomas Steimann <thomas@steimann.ch>
2025-10-17 23:10:42 +0200

    shared button

--- src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
+++ src/app/dashboard/tickets/new-tickets/new-tickets.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from "../../../shared/button/button.component";
 
 @Component({
   selector: 'app-new-tickets',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './new-tickets.component.html',
   styleUrl: './new-tickets.component.css'
 })
